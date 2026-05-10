2c2494b508a45503e9aa70b64b89bbe83e772ad8
mikael <sub2mikael@gmail.com>
2026-05-10 16:19:21 +0800

    Add player kick functionality after delay

---
+++ 1.lua
@@ -0,0 +1,10 @@
+local Players = game:GetService("Players")
+local ren = 5 * 60
+
+Players.PlayerAdded:Connect(function(player)
+    task.delay(ren, function()
+        if player.Parent then
+            player:Kick("you got kicked for 67 reason")
+        end
+    end)
+end)
