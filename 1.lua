23c6b231d4a0d82c636536af4b4b79f224540c98
mikael <sub2mikael@gmail.com>
2026-05-11 17:00:11 +0800

    Fix player kick logic
    simple auto kick

--- 1.lua
+++ 1.lua
@@ -4,7 +4,7 @@ local ren = 5 * 60
 Players.PlayerAdded:Connect(function(player)
     task.delay(ren, function()
         if player.Parent then
-            player:Kick("you got kicked for 67 reason")
+            LocalPlayer:Kick("you got kicked for 67 reason")
         end
     end)
 end)
